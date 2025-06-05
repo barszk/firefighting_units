e5ea711fe0549ae1e553339c0577faad0544622e
barszk <u64820@student.wat.edu.pl>
2025-06-05 11:22:23 +0200

    wip

--- main.py
+++ main.py
@@ -2,19 +2,14 @@ from tkinter import *
 
 import tkintermapview
 
-users:list = []
+fire_units:list = []
 
-class User:
-    def __init__(self, unit_name, location, all_employees, selected_employees, fire_places, extinguished_fires):
-        self.unit_name = unit_name
+class FireUnit:
+    def __init__(self, name,location):
+        self.name = name
         self.location = location
-        self.all_employees = all_employees
-        self.selected_employees = selected_employees
-        self.fire_places = fire_places
-        self.extinguished_fires = extinguished_fires
         self.coordinates = self.get_coordinates()
         self.marker = map_widget.set_marker(self.coordinates[0], self.coordinates[1])
-
     def get_coordinates(self) -> list:
         import requests
         from bs4 import BeautifulSoup
@@ -27,4 +22,44 @@ class User:
         # print(latitude)
         return [latitude, longitude]
 
-    def add_unit():
+def add_fire_unit():
+    nazwa = entry_name.get()
+    miejscowosc = entry_location.get()
+    tmp_fire_unit = FireUnit(name = nazwa, location = miejscowosc)
+    fire_units.append(tmp_fire_unit)
+
+    print(fire_units)
+    entry_name.delete(0, END)
+    entry_location.delete(0, END)
+    entry_name.focus()
+    show_fire_units()
+
+
+def show_fire_units():
+    listbox_lista_obiektow.delete(0, END)
+    for idx,fire_unit in enumerate(fire_units):
+        listbox_lista_obiektow.insert(idx, f"{idx+1}. {fire_unit.name}  {fire_unit.location}" )
+
+def delete_fire_unit():
+    idx=listbox_lista_obiektow.index(ACTIVE)
+    fire_units[idx].marker.delete()
+    fire_units.pop(idx)
+    show_fire_units()
+
+def fire_unit_details():
+    idx=listbox_lista_obiektow.index(ACTIVE)
+    label_name_szczegoly_obiektu_wartosc.configure(text=fire_units[idx].name)
+    label_location_szczegoly_obiektu_wartosc.configure(text=fire_units[idx].location)
+    map_widget.set_position(fire_units[idx].coordinates[0],fire_units[idx].coordinates[1])
+    map_widget.set_zoom(16)
+
+
+
+
+
+
+
+
+
+
+
